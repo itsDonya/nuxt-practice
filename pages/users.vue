60b0b495b17022a9f86926ffe57a8e1f8708c51f
Donya <thisis2nya@gmail.com>
2022-12-25 20:11:37 +0330

    s2: 7. Creating Nested Routes

---
+++ pages/users.vue
@@ -0,0 +1,8 @@
+<template>
+  <article>
+    <h1 class="text-5xl text-center my-4 font-bold text-blue-500">
+      Here you can see our website users
+    </h1>
+    <nuxt-child></nuxt-child>
+  </article>
+</template>
