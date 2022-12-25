a4d0c36fd9c3e26e6cd55e56001cd2cac70f1fe6
Donya <thisis2nya@gmail.com>
2022-12-25 20:55:33 +0330

    s2: 10.Adding a Default Error Page

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="h-screen flex flex-col items-center justify-center gap-8">
+    <h1 class="text-6xl font-bold text-red-700 text-center">
+      An error occurred!
+    </h1>
+    <div class="flex flex-col items-start gap-2">
+      <p class="text-red-700">- Make sure the user exists</p>
+      <p class="text-red-700">- Make sure you entered a correct URL</p>
+    </div>
+    <div class="flex flex-col items-center gap-3">
+      <p class="text-red-500">Any problem?</p>
+      <button
+        class="text-white bg-red-700 rounded-xl px-4 py-2 hover:bg-red-800"
+      >
+        Report Us
+      </button>
+    </div>
+  </section>
+</template>
