ddb67ac8a30b62d91301f502dca71361a767c718
Donya <thisis2nya@gmail.com>
2022-12-29 14:00:35 +0330

    s4: 12.Vuex, fetch() and nuxtServerInit()

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -11,42 +11,10 @@ export default {
   components: {
     TheUser,
   },
-  asyncData(context, callback) {
-    console.log(context);
-    setTimeout(() => {
-      callback(null, {
-        users: [
-          {
-            name: "Donya",
-            lastName: "Davoodi",
-            age: 17,
-            role: "Junior front-end developer",
-            city: "Babolsar",
-            id: "23f8d",
-          },
-          {
-            name: "Arezoo",
-            lastName: "Rezaei",
-            age: 23,
-            role: "Back-end developer",
-            city: "Ahvaz",
-            id: "pa21d",
-          },
-          {
-            name: "Armita",
-            lastName: "Jahani",
-            age: 37,
-            role: "Full-stack developer",
-            city: "Tehran",
-            id: "8c3x1",
-          },
-        ],
-      });
-    }, 3000);
-  },
-  created() {
-    this.$store.dispatch("setUsers", this.users);
-    console.log(this.$store.getters.users);
+  computed: {
+    users() {
+      return this.$store.getters.users;
+    },
   },
 };
 </script>
--- store/index.js
+++ store/index.js
@@ -11,6 +11,40 @@ const createStore = () => {
       },
     },
     actions: {
+      nuxtServerInit(vueContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            // resolve({
+            vueContext.commit("setUsers", [
+              {
+                name: "Donyaaa",
+                lastName: "Davoodi",
+                age: 17,
+                role: "Junior front-end developer",
+                city: "Babolsar",
+                id: "23f8d",
+              },
+              {
+                name: "Arezoo",
+                lastName: "Rezaei",
+                age: 23,
+                role: "Back-end developer",
+                city: "Ahvaz",
+                id: "pa21d",
+              },
+              {
+                name: "Armita",
+                lastName: "Jahani",
+                age: 37,
+                role: "Full-stack developer",
+                city: "Tehran",
+                id: "8c3x1",
+              },
+            ]);
+            resolve();
+          }, 3000);
+        });
+      },
       setUsers(vueContext, users) {
         vueContext.commit("setUsers", users);
       },
